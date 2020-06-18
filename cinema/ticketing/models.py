cb0ec80466537ff5f8366d8aa249fb1ac53b1c2b
Sepehr <sobhani.sepehr@gmail.com>
2020-06-19 01:11:22 +0430

    Movie and Cinema models were added to the ticketing app

--- cinema/ticketing/models.py
+++ cinema/ticketing/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """
+    Represents a movie
+    """
+    name = models.CharField(max_length=100)
+    director = models.CharField(max_length=50)
+    year = models.IntegerField()
+    length = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Cinema(models.Model):
+    """
+    Represents a cinema saloon
+    """
+    cinema_code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=30, default='Tehran')
+    capacity = models.IntegerField()
+    phone = models.CharField(max_length=20, null=True)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
